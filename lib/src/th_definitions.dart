ae7b635dc9c240aa08f18e6624902d1a79c052e7
Rodrigo Severo <rsev@pm.me>
2023-10-30 13:27:07 -0300

    Documenting unicode private-use chars

--- lib/src/th_definitions.dart
+++ lib/src/th_definitions.dart
@@ -5,6 +5,9 @@ const thBackslash = '\\';
 const thLineBreak = '\n';
 const thDoubleQuote = '"';
 const thDoubleQuotePair = r'""';
+
+/// This char is from the private-use chars.
+// See [https://www.unicode.org/faq/private_use.html]
 const thDoubleQuotePairEncoded = '\uE001';
 const thWhitespaceChars = ' \t';
 
