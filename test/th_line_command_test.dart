650469c6467ebd2ff109f3a50a69c82a327e7fb3
Rodrigo Severo <rsev@pm.me>
2023-11-05 11:30:36 -0300

    line -head testing

--- test/th_line_command_test.dart
+++ test/th_line_command_test.dart
@@ -609,6 +609,57 @@ endscrap
     }
   });
 
+  group('line -head', () {
+    final parser = THFileParser();
+    // final grammar = THGrammar();
+    final writer = THFileWriter();
+
+    const successes = [
+      {
+        'file': 'th_file_parser-03030-line_with_head_option.th2',
+        'length': 7,
+        'encoding': 'UTF-8',
+        'asFile': r'''encoding UTF-8
+scrap test
+  line arrow -head both
+    1758 -1030
+    2147.74 -1120.48
+  endline
+endscrap
+''',
+      },
+      {
+        'file': 'th_file_parser-03031-linepoint_with_head_option.th2',
+        'length': 7,
+        'encoding': 'UTF-8',
+        'asFile': r'''encoding UTF-8
+scrap test
+  line arrow -head both
+    1758 -1030
+    2147.74 -1120.48
+  endline
+endscrap
+''',
+      },
+    ];
+
+    for (var success in successes) {
+      test(success, () async {
+        final (file, isSuccessful, _) =
+            await parser.parse((success['file'] as String));
+        // final (file, isSuccessful, errors) = await parser.parse((success['file'] as String),
+        //     startParser: grammar.start());
+        expect(isSuccessful, true);
+        expect(file, isA<THFile>());
+        expect(file.encoding, (success['encoding'] as String));
+        expect(file.countElements(), success['length']);
+
+        final asFile = writer.serialize(file);
+        expect(asFile, success['asFile']);
+      });
+    }
+  });
+
   group('linepoint -close failures', () {
     final parser = THFileParser();
     // final grammar = THGrammar();
