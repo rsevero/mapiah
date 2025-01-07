b7b04931a925ef8283b928e544fe16df2a7a466c
Rodrigo Severo <rsev@pm.me>
2025-01-07 13:02:10 -0300

    Reordering tests

--- test/0200-th_point_command_test.dart
+++ test/0200-th_point_command_test.dart
@@ -1,7 +1,7 @@
 import 'package:mapiah/src/elements/th_element.dart';
 import 'package:mapiah/src/th_file_read_write/th_file_parser.dart';
 import 'package:mapiah/src/th_file_read_write/th_file_writer.dart';
-import 'package:mapiah/src/th_file_read_write/th_grammar.dart';
+// import 'package:mapiah/src/th_file_read_write/th_grammar.dart';
 import 'package:test/test.dart';
 
 import 'th_test_aux.dart';
@@ -61,7 +61,7 @@ endscrap
 
   group('point -align', () {
     final parser = THFileParser();
-    final grammar = THGrammar();
+    // final grammar = THGrammar();
     final writer = THFileWriter();
 
     const successes = [
