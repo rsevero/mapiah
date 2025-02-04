deae02e42cde34891e95ec5b0717f0655d906e27
Rodrigo Severo <rsev@pm.me>
2025-02-04 05:15:28 -0300

    Improving status bar layout

--- lib/src/pages/th2_file_edit_page.dart
+++ lib/src/pages/th2_file_edit_page.dart
@@ -123,67 +123,69 @@ class _TH2FileEditPageState extends State<TH2FileEditPage> {
             },
           ),
           bottomNavigationBar: BottomAppBar(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Observer(
-                      builder: (_) => Text(th2FileEditStore.statusBarMessage)),
-                  Row(
-                    children: [
-                      // ElevatedButton(
-                      //   onPressed: () {
-                      //     // Button action
-                      //   },
-                      //   child: Text('Button 1'),
-                      // ),
-                      // SizedBox(width: 8),
-                      // ElevatedButton(
-                      //   onPressed: () {
-                      //     // Button action
-                      //   },
-                      //   child: Text('Button 2'),
-                      // ),
-                      // SizedBox(width: 8),
-                      // DropdownButton<String>(
-                      //   value: 'Option 1',
-                      //   items: <String>['Option 1', 'Option 2', 'Option 3']
-                      //       .map((String value) {
-                      //     return DropdownMenuItem<String>(
-                      //       value: value,
-                      //       child: Text(value),
-                      //     );
-                      //   }).toList(),
-                      //   onChanged: (String? newValue) {
-                      //     // Dropdown action
-                      //   },
-                      // ),
-                      // Observer(
-                      //   builder: (_) {
-                      //     if (th2FileEditStore.showScrapScale) {
-                      //       return Row(
-                      //         children: [
-                      //           Text(
-                      //             "Scrap scale: ${th2FileEditStore.scrapLengthUnitsPerPointOnScreen.toStringAsFixed(4)} ${th2FileEditStore.scrapLengthUnitType.name} / point",
-                      //           ),
-                      //           SizedBox(width: 8),
-                      //         ],
-                      //       );
-                      //     } else {
-                      //       return const SizedBox();
-                      //     }
-                      //   },
-                      // ),
-                      Observer(
-                        builder: (_) => Text(
-                          "🔍 ${th2FileEditStore.canvasScaleAsPercentageText}",
+            height: 40,
+            color: Theme.of(context).colorScheme.secondary,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Observer(
+                    builder: (_) => Text(th2FileEditStore.statusBarMessage)),
+                Row(
+                  children: [
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     // Button action
+                    //   },
+                    //   child: Text('Button 1'),
+                    // ),
+                    // SizedBox(width: 8),
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     // Button action
+                    //   },
+                    //   child: Text('Button 2'),
+                    // ),
+                    // SizedBox(width: 8),
+                    // DropdownButton<String>(
+                    //   value: 'Option 1',
+                    //   items: <String>['Option 1', 'Option 2', 'Option 3']
+                    //       .map((String value) {
+                    //     return DropdownMenuItem<String>(
+                    //       value: value,
+                    //       child: Text(value),
+                    //     );
+                    //   }).toList(),
+                    //   onChanged: (String? newValue) {
+                    //     // Dropdown action
+                    //   },
+                    // ),
+                    // Observer(
+                    //   builder: (_) {
+                    //     if (th2FileEditStore.showScrapScale) {
+                    //       return Row(
+                    //         children: [
+                    //           Text(
+                    //             "Scrap scale: ${th2FileEditStore.scrapLengthUnitsPerPointOnScreen.toStringAsFixed(4)} ${th2FileEditStore.scrapLengthUnitType.name} / point",
+                    //           ),
+                    //           SizedBox(width: 8),
+                    //         ],
+                    //       );
+                    //     } else {
+                    //       return const SizedBox();
+                    //     }
+                    //   },
+                    // ),
+                    Observer(
+                      builder: (_) => Text(
+                        "🔍 ${th2FileEditStore.canvasScaleAsPercentageText}",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onSecondary,
                         ),
                       ),
-                    ],
-                  ),
-                ],
-              ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         );
