fb02cccfb062c2ba8d0cc9239fcb11d67c9b4f39
Rodrigo Severo <rsev@pm.me>
2025-02-21 14:24:23 +0900

    Adding support for all plataforms

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import open_file_mac
+import package_info_plus
+import path_provider_foundation
+import window_size
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
+}
