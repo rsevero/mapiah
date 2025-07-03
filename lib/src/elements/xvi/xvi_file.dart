5ca3678dd5bb542720bb7731fee003819b270a04
Rodrigo Severo <rsev@pm.me>
2025-07-03 10:41:39 -0300

    XVI: XVIFile initial specification

--- lib/src/elements/xvi/xvi_file.dart
+++ lib/src/elements/xvi/xvi_file.dart
@@ -1,13 +1,25 @@
 import 'package:mapiah/src/elements/parts/th_length_unit_part.dart';
 import 'package:mapiah/src/elements/xvi/xvi_grid.dart';
 import 'package:mapiah/src/elements/xvi/xvi_shot.dart';
+import 'package:mapiah/src/elements/xvi/xvi_sketchline.dart';
 import 'package:mapiah/src/elements/xvi/xvi_station.dart';
 
 class XVIFile {
-  String filename = '';
-  late double gridStartX;
-  late THLengthUnitPart gridStartUnit;
-  List<XVIStation> stations = [];
-  List<XVIShot> shots = [];
-  late XVIGrid grid;
+  String filename;
+  double gridSizeLength;
+  THLengthUnitPart gridSizeUnit;
+  List<XVIStation> stations;
+  List<XVIShot> shots;
+  List<XVISketchLine> sketchLines;
+  XVIGrid grid;
+
+  XVIFile({
+    this.filename = '',
+    required this.gridSizeLength,
+    required this.gridSizeUnit,
+    required this.stations,
+    required this.shots,
+    required this.sketchLines,
+    required this.grid,
+  });
 }
