bfc5e5efe5e82f2d45a9a7b12c1d2a08f23cd3c7
Rodrigo Severo <rsev@pm.me>
2025-07-10 06:29:16 -0300

    XVIFile '==' operator

--- lib/src/elements/xvi/xvi_file.dart
+++ lib/src/elements/xvi/xvi_file.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:mapiah/src/elements/parts/th_length_unit_part.dart';
 import 'package:mapiah/src/elements/parts/types/th_length_unit_type.dart';
 import 'package:mapiah/src/elements/xvi/xvi_grid.dart';
@@ -27,4 +28,29 @@ class XVIFile {
               unit: THLengthUnitType.meter,
             ),
         grid = grid ?? XVIGrid();
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is XVIFile &&
+        filename == other.filename &&
+        gridSizeLength == other.gridSizeLength &&
+        gridSizeUnit == other.gridSizeUnit &&
+        const ListEquality().equals(stations, other.stations) &&
+        const ListEquality().equals(shots, other.shots) &&
+        const ListEquality().equals(sketchLines, other.sketchLines) &&
+        grid == other.grid;
+  }
+
+  @override
+  int get hashCode => Object.hash(
+        filename,
+        gridSizeLength,
+        gridSizeUnit,
+        const ListEquality().hash(stations),
+        const ListEquality().hash(shots),
+        const ListEquality().hash(sketchLines),
+        grid,
+      );
 }
