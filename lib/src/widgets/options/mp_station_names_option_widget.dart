298a0115c716ec1df1249a86df587c0944c90fef
Rodrigo Severo <rsev@pm.me>
2025-08-17 14:30:57 -0300

    mp_station_names_option_widget: migrated to RadioGroup

--- lib/src/widgets/options/mp_station_names_option_widget.dart
+++ lib/src/widgets/options/mp_station_names_option_widget.dart
@@ -181,29 +181,31 @@ class _MPStationNamesOptionWidgetState
           overlayWindowBlockType: MPOverlayWindowBlockType.secondary,
           padding: mpOverlayWindowBlockEdgeInsets,
           children: [
-            RadioListTile<String>(
-              title: Text(appLocalizations.mpChoiceUnset),
-              value: mpUnsetOptionID,
+            RadioGroup<String>(
               groupValue: _selectedChoice,
-              contentPadding: EdgeInsets.zero,
               onChanged: (String? value) {
-                if (value != null) {
-                  _selectedChoice = value;
+                setState(() {
+                  _selectedChoice = value!;
                   _updateIsValid();
+                });
+                if (_selectedChoice == mpNonMultipleChoiceSetID) {
+                  _prefixFieldFocusNode.requestFocus();
                 }
               },
-            ),
-            RadioListTile<String>(
-              title: Text(appLocalizations.mpChoiceSet),
-              value: mpNonMultipleChoiceSetID,
-              groupValue: _selectedChoice,
-              contentPadding: EdgeInsets.zero,
-              onChanged: (String? value) {
-                if (value != null) {
-                  _selectedChoice = value;
-                  _updateIsValid();
-                }
-              },
+              child: Column(
+                children: [
+                  RadioListTile<String>(
+                    title: Text(appLocalizations.mpChoiceUnset),
+                    value: mpUnsetOptionID,
+                    contentPadding: EdgeInsets.zero,
+                  ),
+                  RadioListTile<String>(
+                    title: Text(appLocalizations.mpChoiceSet),
+                    value: mpNonMultipleChoiceSetID,
+                    contentPadding: EdgeInsets.zero,
+                  ),
+                ],
+              ),
             ),
 
             // Additional Inputs for "Set" Option
