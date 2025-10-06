7a586721d03c1513bd3689838bb377b987a513c6
Rodrigo Severo <rsev@pm.me>
2025-10-06 19:27:37 +0200

    Added add scrap test with options and children

--- test/1400-commands_MPAddScrapCommand_test.dart
+++ test/1400-commands_MPAddScrapCommand_test.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mapiah/src/auxiliary/mp_locator.dart';
 import 'package:mapiah/src/controllers/th2_file_edit_controller.dart';
+import 'package:mapiah/src/elements/command_options/th_command_option.dart';
+import 'package:mapiah/src/elements/th_element.dart';
 import 'package:mapiah/src/elements/th_file.dart';
 import 'package:mapiah/src/generated/i18n/app_localizations_en.dart';
 import 'package:mapiah/src/mp_file_read_write/th_file_parser.dart';
@@ -37,36 +40,25 @@ scrap scrap1
 endscrap
 ''',
       },
-      //       {
-      //         'file': '2025-10-06-003-line_mixed.th2',
-      //         'length': 9,
-      //         'encoding': 'UTF-8',
-      //         'asFileOriginal': r'''encoding UTF-8
-      // scrap scrap1 -projection plan
-      //   line wall
-      //     -112.6398 155.7781
-      //     30.6251 132.406
-      //     94.8295 93.0684 99.2447 54.1784 43.8707 15.7361
-      //     -23.5472 -51.5453
-      //   endline
-      // endscrap
-      // ''',
-      //         'asFileChanged': r'''encoding UTF-8
-      // scrap scrap1 -projection plan
-      //   line wall
-      //     -112.6398 155.7781
-      //     -41.00735 144.09205
-      //     30.6251 132.406
-      //     60.439879675246 114.138686903811 77.361588548345 95.967895227729 \
-      //         81.390226619298 77.893634985588
-      //     86.037027138871 57.046023359799 73.530518265772 36.326839908952 43.8707 \
-      //         15.7361
-      //     10.16175 -17.9046
-      //     -23.5472 -51.5453
-      //   endline
-      // endscrap
-      // ''',
-      //       },
+      {
+        'file': '2025-10-06-004-no_scrap.th2',
+        'length': 1,
+        'encoding': 'UTF-8',
+        'asFileOriginal': r'''encoding UTF-8
+''',
+        'asFileChanged': r'''encoding UTF-8
+scrap scrap1 -projection plan -scale [ 0.15 m ]
+  point -2.073 0.042 station
+endscrap
+''',
+        'scrapOptions': [
+          r'{"optionType":"projection","parentMPID":0,"originalLineInTH2File":"","mode":"plan","index":""}',
+          r'{"optionType":"scrapScale","parentMPID":0,"originalLineInTH2File":"","numericSpecifications":[{"partType":"double","value":0.15,"decimalPositions":2}],"unit":{"partType":"lengthUnit","unit":"m"}}',
+        ],
+        'scrapChildren': [
+          '{"elementType":"point","mpID":4,"parentMPID":0,"sameLineComment":null,"originalLineInTH2File":"","position":{"partType":"position","coordinates":{"dx":-2.073076837713069,"dy":0.04164428710937518},"decimalPositions":3},"pointType":"station","unknownPLAType":"","optionsMap":{},"attrOptionsMap":{}}',
+        ],
+      },
     ];
 
     for (var success in successes) {
@@ -100,7 +92,26 @@ endscrap
 
             controller.setCanvasScale(1);
 
-            controller.elementEditController.createScrap(thID: 'scrap1');
+            final List<THCommandOption>? scrapOptions =
+                (success['scrapOptions'] != null)
+                ? (success['scrapOptions'] as List<dynamic>)
+                      .map(
+                        (s) => THCommandOption.fromMap(jsonDecode(s as String)),
+                      )
+                      .toList()
+                : null;
+            final List<THElement>? scrapChildren =
+                (success['scrapChildren'] != null)
+                ? (success['scrapChildren'] as List<dynamic>)
+                      .map((s) => THElement.fromMap(jsonDecode(s as String)))
+                      .toList()
+                : null;
+
+            controller.elementEditController.createScrap(
+              thID: 'scrap1',
+              scrapChildren: scrapChildren,
+              scrapOptions: scrapOptions,
+            );
 
             final String asFileChanged = writer.serialize(controller.thFile);
 
